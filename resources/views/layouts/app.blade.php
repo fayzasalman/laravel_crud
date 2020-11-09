f90405b8b42ca54dd318488e5f5adc5d2bf744c2
faeza97 <princessfayza97@gmail.com>
2020-11-09 16:23:05 +0300

    Done with this task

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,11 +18,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css"
+        rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
-
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/switchery/0.8.2/switchery.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/switchery/0.8.2/switchery.min.js"></script> -->
 
     <!-- its for the swal alert -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
@@ -94,28 +92,30 @@
 </body>
 <script type="text/javascript">
 $(document).ready(function() {
-        $('.toggle').click(function() {
-            var status = $(this).children().prop('checked') == true ? 'inactive' : 'active';
-            var id = $(this).children().data('id');
-            $.ajax({
-                type: 'POST',
-                dataType: "json",
-                url: 'statusUpdate',
-                data: {
-                    'status': status,
-                    'id': id
-                },
-                headers: {
-                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                },
-                success: function(data) {
-                    Swal.fire(
-                        'GREAT!', 'Status changed successfully', 'success')
-                        location.reload();
-                    console.log(data.success);
-                }
-            });
+    $('.toggle').click(function() {
+        var status = $(this).children().prop('checked') == true ? 'inactive' : 'active';
+        var id = $(this).children().data('id');
+        $.ajax({
+            type: 'POST',
+            dataType: "json",
+            url: 'statusUpdate',
+            data: {
+                'status': status,
+                'id': id
+            },
+            headers: {
+                'X-CSRF-TOKEN': '{{ csrf_token() }}'
+            },
+            success: function(data) {
+                Swal.fire(
+                    "GREAT!",
+                    "Status changed successfully",
+                    "success");
+                location.reload();
+                console.log(data.success);
+            }
         });
+    });
 })
 </script>
 
